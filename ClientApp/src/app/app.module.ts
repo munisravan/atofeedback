fc1b870bf0ccec73f8484387042ead21af59ec89
munisravan <muni.sravan@gmail.com>
2018-09-09 15:38:16 +0930

    added team and video pages

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,6 +12,9 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProjectExplantionComponent } from './project-explantion/project-explantion.component';
+import { MaterialModule } from './shared/material.module';
+import { TeamComponent } from './team/team.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
@@ -18,15 +23,21 @@ import { ProjectExplantionComponent } from './project-explantion/project-explant
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    ProjectExplantionComponent
+    ProjectExplantionComponent,
+    TeamComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    BrowserAnimationsModule,
     FormsModule,
+    MaterialModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'project', component: ProjectExplantionComponent }
+      { path: 'video', component: VideoComponent }
+      { path: 'team', component: TeamComponent }
     ])
   ],
   providers: [],
---
+++ ClientApp/src/app/shared/material.module.ts
@@ -0,0 +1,80 @@
+import { NgModule } from '@angular/core';
+import {MatRippleModule} from '@angular/material';
+import { CdkTableModule } from '@angular/cdk/table';
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+  MatNativeDateModule,
+  MatDividerModule
+} from '@angular/material';
+
+@NgModule({
+      exports: [
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatTableModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSlideToggleModule,
+        MatSliderModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatStepperModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatNativeDateModule,
+        CdkTableModule,
+        MatDividerModule
+      ],
+})
+export class MaterialModule {
+
+}
